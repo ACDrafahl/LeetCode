34c298d3951c209aec111c4b54e1bc2c23b27fc9
Andy Drafahl <118315981+ACDrafahl@users.noreply.github.com>
2023-12-21 16:26:45 -0600

    Time: 7 ms (23.83%), Space: 42.9 MB (7.16%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,24 +1,23 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int index1 = 0;
-        int index2 = 0;
+        int index = 0;
+
         String mergedString = "";
-        while(index1 < word1.length() && index2 < word2.length()) {
-            mergedString = mergedString + word1.charAt(index1);
-            mergedString = mergedString + word2.charAt(index2);
-            index1++;
-            index2++;
+        while(index < word1.length() && index < word2.length()) {
+            mergedString = mergedString + word1.charAt(index);
+            mergedString = mergedString + word2.charAt(index);
+            index++;
         }
-        if (index1 < word1.length()) {
-            while (index1 < word1.length()) {
-                mergedString = mergedString + word1.charAt(index1);
-                index1++;
+        if (index < word1.length()) {
+            while (index < word1.length()) {
+                mergedString = mergedString + word1.charAt(index);
+                index++;
             }
         }
-        if (index2 < word2.length()) {
-            while (index2 < word2.length()) {
-                mergedString = mergedString + word2.charAt(index2);
-                index2++;
+        if (index < word2.length()) {
+            while (index < word2.length()) {
+                mergedString = mergedString + word2.charAt(index);
+                index++;
             }
         }
         return mergedString;
