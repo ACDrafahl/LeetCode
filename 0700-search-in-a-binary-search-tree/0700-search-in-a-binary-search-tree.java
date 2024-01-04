e32e5eb3328f24d98ea682d6d0c42af90a2c6cc3
Andy Drafahl <118315981+ACDrafahl@users.noreply.github.com>
2024-01-04 10:41:09 -0600

    Time: 0 ms (100.00%), Space: 45.7 MB (5.19%) - LeetHub

---
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        //Recursive solution
+        if(val == root.val) { //base case
+            return root;
+        }
+        else if(val < root.val && root.left != null) {
+            return searchBST(root.left, val);
+        }
+        else if(root.right != null) {
+            return searchBST(root.right, val);
+        }
+        return null;
+        
+        /* 
+        //Iterative solution
+        //O(n) for n elements in stack
+        Stack<TreeNode> tree = new Stack<>();
+        
+        tree.push(root);
+
+        //O(n) for n elements in tree in worst case
+        while(!tree.isEmpty()) {
+            TreeNode temp = tree.pop();
+
+            if(val == temp.val) {
+                return temp;
+            }
+            else if(val < temp.val && temp.left != null) {
+                tree.push(temp.left);
+            }
+            else if(temp.right != null) {
+                tree.push(temp.right);
+            }
+        }
+
+        return null; 
+        */
+    }
+}
